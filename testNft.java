aa557a3081f6986330c950280c0659a20765633f
Rubi Mirza <rubimirza647@gmail.com>
2024-08-06 13:16:02 +0800

    Create testNft.java

---
+++ testNft.java
@@ -0,0 +1,36 @@
+ @Test
+    void testNftMetadataUpload() {
+        String nftId = "184734734873";
+        var metadata = NftMetadata.builder()
+                .name("Hello World #" + nftId)
+                .image("https://wwww.google.com")
+                .description("Java: A popular language")
+                .properties(NftMetadata.NftProperties.builder()
+                        .bizId("111111")
+                        .authorId("32131321")
+                        .mintedBySocrates("true")
+                        .fileType("video")
+                        .media(List.of(
+                                NftMetadata.NftMedia.builder().type(MediaTypeEnum.Video.getValue())
+                                        .imageUrl("https://www.aa.com")
+                                        .fileUrl("https://www.aa.com")
+                                        .build(),
+                                NftMetadata.NftMedia.builder().type(MediaTypeEnum.Audio.getValue())
+                                        .imageUrl("https://www.aa.com")
+                                        .fileUrl("https://www.aa.com")
+                                        .build(),
+                                NftMetadata.NftMedia.builder().type(MediaTypeEnum.Image.getValue())
+                                        .imageUrl("https://www.aa.com")
+                                        .fileUrl("https://www.aa.com")
+                                        .build(),
+                                NftMetadata.NftMedia.builder().type(MediaTypeEnum.File.getValue())
+                                        .imageUrl("https://www.aa.com")
+                                        .fileUrl("https://www.aa.com")
+                                        .build()
+                        ))
+                        .build())
+                .build();
+        var result = nftMetadataService.upload(nftId, metadata);
+        log.info("s3 metadata url is: {}", result);
+
+    }
